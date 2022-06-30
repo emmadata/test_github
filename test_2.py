b52eec166d6637840dfe873c4a3d42da0471d2ab
emmadata <helloemmadata@gmail.com>
2022-06-30 22:07:33 +0200

    add function print_its_magic

--- test_2.py
+++ test_2.py
@@ -4,3 +4,7 @@ print("hello, i'm testing github")
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+def print_its_magic():
+    print("It's soo magic!")
