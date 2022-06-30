9fad021d4a0b2994917e47d927bd361dbe4965fc
emmadata <helloemmadata@gmail.com>
2022-06-30 22:03:54 +0200

    add function print_hi

--- test_2.py
+++ test_2.py
@@ -1,1 +1,6 @@
 print("hello, i'm testing github")
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
